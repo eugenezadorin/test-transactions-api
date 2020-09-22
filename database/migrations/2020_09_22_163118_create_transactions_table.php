d7c8db9d051f6614d6a08908e5da42f0b79e1153
Eugene Zadorin <mail@eugenezadorin.ru>
2020-09-22 20:49:01 +0300

    add basic migrations and setup guide

---
+++ database/migrations/2020_09_22_163118_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id');
+            $table->string('type', 50);
+            $table->decimal('amount', 9, 4);
+            $table->string('currency', 3);
+            $table->string('reason', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
