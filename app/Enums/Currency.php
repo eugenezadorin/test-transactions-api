fead7844dd44c4a06fe69abea03a50ff6f45d79d
Eugene Zadorin <mail@eugenezadorin.ru>
2020-09-22 21:40:47 +0300

    add enums dependency and make some basic enums

---
+++ app/Enums/Currency.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use \Spatie\Enum\Enum;
+
+/**
+ * @method static self RUB()
+ * @method static self USD()
+ */
+class Currency extends Enum
+{
+}
---
+++ app/Enums/TransactionType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use \Spatie\Enum\Enum;
+
+/**
+ * @method static self debit()
+ * @method static self credit()
+ */
+class TransactionType extends Enum
+{
+}
